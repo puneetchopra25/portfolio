79146d4e51c48615fcc06caa8b9fbefc3c9452aa
puneetchopra25 <puneetchopra2002@gmail.com>
2024-05-01 23:38:12 -0700

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-
-function App() {
+import Header from "./components/Header";
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline bg-cyan-300">
-      Hello world!
-    </h1>
-  )
+    <>
+      <div style={{
+        background: "linear-gradient(to bottom right, black 40%, #1D0A53 100%)",
+        minHeight: "100vh"
+        }}
+      >
+        <Header />
+        <h1 className="text-3xl font-bold underline text-cyan-50">
+          Hello world!
+        </h1>
+      </div>
+    </>
+    
+  );
 }
 export default App
